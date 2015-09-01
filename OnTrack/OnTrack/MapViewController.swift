d2c3324f7304e334584bac87b4b7f257ccdea512
LondonSwift <hello@LondonSwift.com>
2015-09-01 16:42:01 +0100

    outlets connected

---
+++ OnTrack/OnTrack/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  OnTrack
+//
+//  Created by Daren David Taylor on 01/09/2015.
+//  Copyright (c) 2015 LondonSwift. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapTypeButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var allButton: UIButton!
+    @IBOutlet weak var youButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
+
+
+
